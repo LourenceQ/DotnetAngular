239924b5c0a0eaa9f5da710fbd3fbf4faf714105
LourenceQ <lqfmecatronica@gmail.com>
2021-11-24 16:24:42 -0300

    Injetando StoreContextx em productsController. criando Constructor de ProductController para inicializar instancia de StoreContext

--- API/Startup.cs
+++ API/Startup.cs
@@ -33,10 +33,10 @@ namespace API
             services.AddControllers();
             services.AddDbContext<StoreContext>(x => 
                 x.UseSqlite(_configuration.GetConnectionString("DefaultConnection")));
-            // services.AddSwaggerGen(c =>
-            // {
-            //     c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
-            // });
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
