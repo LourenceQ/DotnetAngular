63cd70cb35461c9d91c067fbcb515ddb7f304a0f
LourenceQ <lqfmecatronica@gmail.com>
2021-11-29 16:53:25 -0300

    Incluindo na classe Program.cs método statico SeedAsync de classe StoreCOntextSeed para popular db ao inicializar a plicaçao

--- Infrastructure/Data/StoreContextSeed.cs
+++ Infrastructure/Data/StoreContextSeed.cs
@@ -29,29 +29,29 @@ namespace Infrastructure.Data
                     await context.SaveChangesAsync();
                 }
 
+                if(!context.ProductTypes.Any())
+                {
+                    var typesData = File.ReadAllText("../Infrastructure/Data/SeedData/types.json");
+
+                    var types = JsonSerializer.Deserialize<List<ProductType>>(typesData);
+
+                    foreach(var item in types)
+                    {
+                        context.ProductTypes.Add(item);
+                    }
+
+                    await context.SaveChangesAsync();
+                }
+
                 if(!context.Products.Any())
                 {
-                    var productsData = File.ReadAllText("../Infrastructure/Data/SeedData/btypesrands.json");
+                    var productsData = File.ReadAllText("../Infrastructure/Data/SeedData/products.json");
 
                     var products = JsonSerializer.Deserialize<List<Product>>(productsData);
 
                     foreach(var item in products)
                     {
                         context.Products.Add(item);
-                    }
-
-                    await context.SaveChangesAsync();
-                }
-
-                if(!context.ProductBrands.Any())
-                {
-                    var brandsData = File.ReadAllText("../Infrastructure/Data/SeedData/brands.json");
-
-                    var brands = JsonSerializer.Deserialize<List<ProductBrand>>(brandsData);
-
-                    foreach(var item in brands)
-                    {
-                        context.ProductBrands.Add(item);
                     }
 
                     await context.SaveChangesAsync();
