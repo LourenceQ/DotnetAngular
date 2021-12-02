bc88ef9e2c56666224bb4ac1323799737098e6f0
LourenceQ <lqfmecatronica@gmail.com>
2021-12-02 09:23:33 -0300

    Implementando métodos de especificações em GenericRepository

--- Core/Interfaces/IGenericRepository.cs
+++ Core/Interfaces/IGenericRepository.cs
@@ -11,7 +11,7 @@ namespace Core.Interfaces
     {
         Task<T> GetByIdAsync(int id);
         Task<IReadOnlyList<T>> ListAllAsync();
-        Task<T> GetEnriryWithSpec(ISpecification<T> spec);
+        Task<T> GetEntityWithSpec(ISpecification<T> spec);
         Task<IReadOnlyList<T>> ListAsync(ISpecification<T> spec);
     }
 }
