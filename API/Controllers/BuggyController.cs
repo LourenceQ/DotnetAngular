d87b90824d865177903720143abdcc7bf788f16b
LourenceQ <lqfmecatronica@gmail.com>
2021-12-07 15:13:14 -0300

    Implementando base controller

---
+++ API/Controllers/BuggyController.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    public class BuggyController : BaseApiController
+    {
+        private readonly StoreContext _context;
+        public BuggyController(StoreContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("notfound")]
+        public ActionResult GetNotFoundRequest()
+        {
+            return Ok();
+        }
+        
+        
+    }
+}
