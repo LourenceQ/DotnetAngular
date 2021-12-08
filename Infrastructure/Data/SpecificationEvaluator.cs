57654884a8bee52ec77eff64ecabc9c149229c57
LourenceQ <lqfmecatronica@gmail.com>
2021-12-08 10:11:42 -0300

    Criando expressão na classe de especificação para executar Sorting dos produtos

--- Infrastructure/Data/SpecificationEvaluator.cs
+++ Infrastructure/Data/SpecificationEvaluator.cs
@@ -20,6 +20,17 @@ namespace Infrastructure.Data
                 query = query.Where(spec.Criteria);
             }
 
+            if (spec.OrderBy != null)
+            {
+                query = query.OrderBy(spec.OrderBy);
+            }
+
+            if (spec.OrderByDescending != null)
+            {
+                query = query.OrderByDescending(spec.OrderByDescending);
+            }
+
+
             query = spec.Includes.Aggregate(query, (currrent, include) => currrent.Include(include));
             
             return query;
