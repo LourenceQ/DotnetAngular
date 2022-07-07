d97e86607f5ff44c1652e78cce6ac6ae1f47fe7b
LourenceQ <lqfmecatronica@gmail.com>
2022-07-06 21:34:01 -0300

    enabling linear mode

--- API/Startup.cs
+++ API/Startup.cs
@@ -14,12 +14,13 @@ namespace API
     public class Startup
     {
         private readonly IConfiguration _configuration;
+        
         public Startup(IConfiguration configuration)
         {
             _configuration = configuration;
         }
 
-        public IConfiguration Configuration { get; }
+        // public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
